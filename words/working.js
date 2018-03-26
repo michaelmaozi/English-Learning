ff86bc24fc3c6d602cb35f9bdabb0fe53334af3d
michaelmaozi <michaelmao.zi@gmail.com>
2018-03-26 14:19:24 -0400

    created and added words

---
+++ words/working.js
@@ -0,0 +1,72 @@
+{
+	"falter"     : {
+		"a" : "['fɔ:ltə]. n.踌躇；支吾；颤抖. vi. 支吾；蹒跚地走",
+		"b" : "He is not likely to falter on such an occasion."
+	},
+
+	"expressive" : {
+		"a" : "[ɪk'sprɛsɪv]. adj. 表现的；有表现力的；表达…的",
+		"b" : "We are creative, expressive, knowledge-seeking, and able to obey reason."
+	},
+
+	"extraordinarily" : {
+		"a" : "[ɪk'strɔdɪn(ə)rəlɪ]. adv. 非常；格外地；非凡地",
+		"b" : "I am extraordinarily grateful."
+	},
+
+	"eliminates": { 
+		"a" : "[ɪˈlɪmɪˌneɪt]. vt 消除",
+		"b" : "That eliminates all risk for me from the get go."
+	},
+
+	"bloated" : {
+		"a" : "['blotɪd]. adj. 发胀的，浮肿的；傲慢的. v. 肿胀，得意（bloat的过去时形式",
+		"b" : "But stars like our sun swell into bloated red giants when the nuclear fuel in their cores is depleted."
+	},
+
+	"simplicity" : {
+		"a" : "[sɪm'plɪsəti]. n. 朴素；简易；天真；愚蠢",
+		"b" : "I like simplicity in life."
+	},
+
+	"grateful" : {
+		"a" : "['ɡretfl]. adj. 感谢的；令人愉快的，宜人的",
+		"b" : "Our grateful thanks go to all who participated."
+	},
+
+	"fell" : {
+		"a" : "[fɛl]. adj. 凶猛的；毁灭性的. vt. 砍伐；打倒；击倒. n. [林] 一季所伐的木材；折缝；兽皮. v. 掉下；摔倒；下垂；变坏（fall的过去式",
+		"b" : "He fell from the ladder."
+	},
+
+	"publicly" : {
+		"a" : "['pʌblikli]. adv. 公然地；以公众名义",
+		"b" : "A company that is publicly owned has sold its share s to people who are not part of the company"
+	},
+
+	"juggling" : {
+		"a" : "['dʒʌglɪŋ]. adj. 欺诈的；变戏法的；欺骗的. n. 杂耍；欺骗. v. 玩杂耍（juggle的ing形式）",
+		"b" : "At home, one of her siblings was in the back yard doing something like juggling with screwdrivers of different sizes."
+	},
+
+	"numerous" : {
+		"a" : "adj. 许多的，很多的",
+		"b" : "As a blogger you have numerous way to do it."
+	},
+
+	"substantial" : {
+		"a" : "[səbˈstænʃəl]. adj. 大量的；实质的；内容充实的. n. 本质；重要材料",
+		"b" : "We have the support of a substantial number of parents."
+	},
+
+	"wildcards" : {
+		"a" : "n. 通配符；万用字符 *",
+		"b" : "A lot of people try to avoid moving to this contract maturity level by extensive use of wildcards to avoid versioning."
+	},
+
+	"Insights" : {
+		"a" : "n. 洞察力；眼力；深刻见解（insight的复数)",
+		"b" : "Certain objects could help us in doing so by providing insights and surprises."
+	},
+	
+}
